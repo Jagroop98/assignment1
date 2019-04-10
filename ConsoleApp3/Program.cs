25c242bf2fda6bf9aa62ab776ddae01227c56d21
visualstudio <visualstudio@122-PC17>
2019-04-10 11:01:30 -0400

    add

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -20,3 +20,25 @@ namespace ConsoleApp3
         public string getVillageName() { return villageName; }
     }
 }
+
+class Hugi
+{
+    private static JournalEntry je;
+    public static bool FoundAstrilde = false;
+
+    // TO DO
+
+    public static List<JournalEntry> HugiJournal = new List<JournalEntry>();
+
+    public static int CalculateDistanceWalked()
+    {
+        int DistanceWalked = 0;
+        // walk over the List and add the distances
+        foreach (var je in HugiJournal)
+        {
+            Console.WriteLine(" {0}  --   {1} *** --- {2} ", je.getDistanceWalked(), je.getVillageName(), je.HowFarToGetBack);
+            DistanceWalked += je.getDistanceWalked() + je.HowFarToGetBack;
+        }
+        return DistanceWalked;
+    }
+}
