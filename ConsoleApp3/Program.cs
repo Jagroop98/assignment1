b8b3ad3ec30ba3b3198af04035f7e3bd2fde1a69
visualstudio <visualstudio@122-PC17>
2019-04-10 11:06:01 -0400

    jjj

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -112,3 +112,30 @@ public void Run()
     this.TraverseVillages(Alst);
     this.Announcement();
 }
+public void Announcement()
+{
+    string filename = "U:/Users/730263/a.txt";
+    try
+    {
+        // Create an instance of StreamReader to read from a file.
+        // The using statement also closes the StreamReader.
+        using (StreamReader sr = new StreamReader(filename))
+        {
+            string line;
+
+            // Read and display lines from the file until 
+            // the end of the file is reached. 
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+    catch (Exception e)
+    {
+        // Let the user know what went wrong.
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+}
